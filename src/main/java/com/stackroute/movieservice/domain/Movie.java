656b8791c222cf24d5b86a2dc431abe9f7ea25dc
Sai Saketh Maringanti <saketh090597@gmail.com>
2018-08-04 14:52:00 +0530

    initial commit

--- src/main/java/com/stackroute/movieservice/domain/Movie.java
+++ src/main/java/com/stackroute/movieservice/domain/Movie.java
@@ -1,14 +1,15 @@
 package com.stackroute.movieservice.domain;
 
-import org.springframework.data.annotation.Id;
-import org.springframework.data.mongodb.core.mapping.Document;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
 @Builder
-@Document
+@Entity
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
